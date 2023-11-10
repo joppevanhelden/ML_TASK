21897dd525ade2b512761359d16c473f9e5a483d
Joppe Vanhelden <91123515+joppevanhelden@users.noreply.github.com>
2023-11-10 21:20:35 +0100

    Add files via upload

--- ML_Task.py
+++ ML_Task.py
@@ -11,36 +11,35 @@ from ucimlrepo import fetch_ucirepo
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# Step 1: Load the dataset and perform EDA
+# load the dataset
 glass_identification = fetch_ucirepo(id=42)
 X = glass_identification.data.features
 y = glass_identification.data.targets
 
-# Step 2: Split the data into training and test sets
+# split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Step 3: Train the baseline model using Random Forest
+# train the baseline model w random forest
 rf_model = RandomForestClassifier()
 rf_model.fit(X_train, y_train)
 
-# Step 4: Train SVM and KNN models
+# train SVM and KNN models
 svm_model = SVC()
 svm_model.fit(X_train, y_train)
 
 knn_model = KNeighborsClassifier()
 knn_model.fit(X_train, y_train)
 
-# Streamlit app
-st.title("Task Machine Learning: Benchmarking - Joppe Vanhelden")
+st.title("Task Machine Learning: Benchmarking")
 
-# Sidebar controls
+# sidebar
 selected_models = st.sidebar.multiselect("Select Models", ["Random Forest", "SVM", "KNN"])
 compare_confusion_matrices = st.sidebar.checkbox("Compare Confusion Matrices")
 
-# Main content
 for selected_model in selected_models:
     st.subheader(f"{selected_model} Performance:")
 
+    # the different ML techniques
     if selected_model == "Random Forest":
         model = rf_model
     elif selected_model == "SVM":
@@ -58,7 +57,7 @@ for selected_model in selected_models:
         conf_matrix = confusion_matrix(y_test, y_pred)
         st.subheader(f"{selected_model} Confusion Matrix:")
 
-        # Plot Confusion Matrix with different color map for each model
+        # Plot confusion matrix
         plt.figure(figsize=(9, 7))
         cmap = "Blues" if selected_model == "Random Forest" else "Greens" if selected_model == "SVM" else "Reds"
         sns.heatmap(conf_matrix, annot=True, fmt="d", cmap=cmap, cbar=False)
@@ -67,16 +66,11 @@ for selected_model in selected_models:
         plt.ylabel("True Label")
         st.pyplot()
 
-# EDA Visualization
+# EDA visualization: bar chart 'Type_of_glass'
 st.subheader("Exploratory Data Analysis (EDA) Visualization:")
-# Bar chart to show the distribution of 'Type_of_glass'
 plt.figure(figsize=(8, 6))
 sns.countplot(x='Type_of_glass', data=glass_identification.data.targets)
 plt.title('Distribution of Glass Types')
 plt.xlabel('Type of Glass')
 plt.ylabel('Count')
 st.pyplot()
-
-# Additional sections for other comparisons or analyses can be added as needed
-
-# Run the app with `streamlit run your_app_name.py` in the terminal
